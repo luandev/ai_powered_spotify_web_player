e848fdd47cd8c2d2e8f3239441eae6cc6a3bbbbf
Luan Citta <dev.luan.citta@gmail.com>
2025-01-04 10:42:05 +0000

    spotify grant front end

--- src/app/_components/spotify_connect.tsx
+++ src/app/_components/spotify_connect.tsx
@@ -1,25 +1,24 @@
 'use client'
 
 import { useState, useEffect } from "react";
-import { AuthorizationCodeWithPKCEStrategy, SpotifyApi, UserProfile } from "@spotify/web-api-ts-sdk";
+import { AuthorizationCodeWithPKCEStrategy, SpotifyApi, UserProfile, PlaybackState } from "@spotify/web-api-ts-sdk";
 
 const SpotifyConnect = () => {
   const [profile, setProfile] = useState<UserProfile | null>(null);
+  const [playbackState, setPlaybackState] = useState<PlaybackState | null>(null);
 
   useEffect(() => {
     const implicitGrantStrategy = new AuthorizationCodeWithPKCEStrategy(
       process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
       `http://localhost:3000/radio`,
-      ['user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private, user-read-playback-state, user-modify-playback-state']
+      ['user-read-private', 'user-read-email', 'playlist-modify-public', 'playlist-modify-private', 'user-read-playback-state', 'user-modify-playback-state']
     );
 
     const spotify = new SpotifyApi(implicitGrantStrategy);
-    const fetchProfile = async () => {
-      const profile = await spotify.currentUser.profile();
-      setProfile(profile);
-    };
-
-    fetchProfile();
+    (async () => {
+      setProfile(await spotify.currentUser.profile());
+      setPlaybackState(await spotify.player.getPlaybackState());
+    })();
   }, []);
 
   return (
@@ -31,7 +30,16 @@ const SpotifyConnect = () => {
           <p>{profile.email}</p>
         </div>
       ) : (
-        <p>Loading...</p>
+        <p>Loading profile...</p>
+      )}
+      {playbackState ? (
+        <div>
+          <h3>Currently Playing</h3>
+          <p>{playbackState.item.name} by {playbackState.item.artists.map(artist => artist.name).join(', ')}</p>
+          <img src={playbackState.item.album.images[0]?.url} alt="Album Art" />
+        </div>
+      ) : (
+        <p>Loading currently playing...</p>
       )}
     </div>
   );
